384dc059b0c0b35a9c9641cb1bea5cc9ead35461
kostya92-ку <kostya.sukharskiy@gmail.com>
2020-06-02 00:05:50 +0300

    homwork-5

---
+++ Module-5/js.js/task-1.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const Account = function ({ email, login }) {
+  this.email = email;
+  this.login = login;
+};
+Account.prototype.getInfo = function () {
+  const message = `Login: ${this.login}, Email: ${this.email}`;
+  console.log(message);
+};
+
+console.log(Account.prototype.getInfo);
+const mango1 = new Account({
+  login: 'Mangozedog',
+  email: 'mango@dog.woof',
+});
+mango1.getInfo();
+const poly1 = new Account({
+  login: 'Poly',
+  email: 'poly@mail.com',
+});
+poly1.getInfo();
---
+++ Module-5/js.js/task-3.js
@@ -0,0 +1,35 @@
+'use strict';
+
+class Storage {
+  constructor(arr = []) {
+    this.arr = arr;
+  }
+  addItem(item) {
+    this.arr.push(item);
+  }
+  getItems() {
+    return this.arr;
+  }
+  removeItem(item) {
+    let res = this.arr.includes(item);
+    if (res) {
+      let ind = this.arr.indexOf(item);
+      let remov = this.arr.splice(1, 1);
+    }
+  }
+}
+
+const storage = new Storage([
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор',
+]);
+
+const items = storage.getItems();
+
+console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+storage.addItem('Дроид');
+console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+storage.removeItem('Пролонгер');
+console.table(items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
