5bde8e62afc5f188615f8b047bbf429856fd3365
kostya92-ку <kostya.sukharskiy@gmail.com>
2020-06-14 10:52:39 +0300

    homwork-6

---
+++ Module-6/js.js/task-10.js
@@ -0,0 +1,12 @@
+"use strict";
+import users from "./users.js";
+const getSortedUniqueSkills = (users) =>
+    users
+    .reduce((allSkills, current) => {
+        allSkills.push(...current.skills);
+        return allSkills;
+    }, [])
+    .sort()
+    .filter((elem, index, array) => index === array.indexOf(elem));
+
+console.table(getSortedUniqueSkills(users));
